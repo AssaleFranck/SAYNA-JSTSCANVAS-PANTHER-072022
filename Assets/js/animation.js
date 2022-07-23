87138e96be66bc28d21b518ca942f36fbf77b4bf
AssaleFranck <franck7hermanntanoh@gmail.com>
2022-07-23 13:04:16 +0000

    add game js

--- Assets/js/animation.js
+++ Assets/js/animation.js
@@ -10,25 +10,26 @@ $(document).ready(function () {
             if ($(this).offset().top > scrollPosition && $(this).offset().top < scrollPosition + viewportHeight) {
                 
 
-                let id = null;
-                const elem = this;
-                // const elem = document.getElementById("animate");
-                let pos = 300;
-                clearInterval(id);
-                // id = setInterval(frame, 5);
-                // id = setTimeout(frame, 500);
-                function frame() {
-                    if (pos == 0) {
-                    clearInterval(id);
-                    } else {
-                    pos--; 
-                    elem.style.right = pos + "px";
-                    }
-                }
-
-                // function donne () {  
-                //     $('.container').css('display', 'flex');
+                // let id = null;
+                // const elem = this;
+                // // const elem = document.getElementById("animate");
+                // let pos = 300;
+                // clearInterval(id);
+                // // id = setInterval(frame, 5);
+                // // id = setTimeout(frame, 500);
+                // function frame() {
+                //     if (pos == 0) {
+                //     clearInterval(id);
+                //     } else {
+                //     pos--; 
+                //     elem.style.right = pos + "px";
+                //     }
                 // }
+
+                function donne () {  
+                    // $('.container').css('display', 'flex');
+                    $('.fadein').fadeIn('slow');
+                }
                 // setTimeout(donne, 1000)
             }
             
